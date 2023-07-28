2679f013f927aaa87f04b96d5b13b2b43b898e3c
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-07-28 13:35:27 +0530

    first commit

--- client/src/pages/trending_sec/trendingCard.jsx
+++ client/src/pages/trending_sec/trendingCard.jsx
@@ -7,32 +7,33 @@ const TrendingCard = ({ products }) => {
 
     return (
 
-           <div className="product-card py-1" style={{background:"white", border:"1px solid #edecec"}}>
+        <div className="product-card py-1" style={{ background: "white", border: "1px solid #edecec" }}>
             {/* trending card */}
-            <div className="product-tumb" style={{background:"white", padding:"2px", maxHeight:"90%"}}>
-            <img style={{ width: "90%", height:"90%" }} src={products.images[0]?.url} alt={products.title} />
+            <div className="product-tumb" style={{ background: "white", padding: "2px", maxHeight: "90%" }}>
+                <img style={{ width: "90%", height: "90%" }} src={products.images[0]?.url} alt={products.title} />
             </div>
 
-                <div className="product-details" style={{height:"80%", padding:"3px"}} >
-                <div className="clearfix mb-1 d-flex flex-column" style={{width:"100%"}}>
-                    <span title={products.title} className="product-catagory m-0 text-start px-2 rounded-pill bg-primary" style={{width:"max-content", fontSize:"60%", height:"22px", maxWidth:"100%", display:"-webkit-box", overflow:"hidden", WebkitLineClamp:"1", WebkitBoxOrient:"vertical"}}>{products.title}</span>
-                    <strong className="float-end price-hp mt-1 p-0 px-1" style={{ fontSize: "70%", width:"max-content", alignSelf:"end" }}>₹{products.price}/-</strong>
+            <div className="product-details" style={{ height: "80%", padding: "3px" }} >
+                <div className="clearfix mb-1 d-flex flex-column" style={{ width: "100%" }}>
+                    {/* <span title={products.title} className="product-catagory m-0 text-start px-2 rounded-pill bg-primary" style={{width:"max-content", fontSize:"70%", height:"max-content", maxWidth:"100%", display:"-webkit-box", overflow:"hidden", WebkitLineClamp:"1", WebkitBoxOrient:"vertical"}}>{products.title}</span> */}
+                    <h5 className="card-title p-0 px-1 m-0 text-start rounded-pill bg-primary" style={{ fontSize: "90%", width: "max-content", maxWidth:"100%", display: "-webkit-box", overflow: "hidden", WebkitLineClamp: "1", WebkitBoxOrient: "vertical", color: "white" }}>{products.title}</h5>
+                    <strong className="float-end price-hp mt-1 p-0 px-1" style={{ fontSize: "70%", width: "max-content", alignSelf: "end" }}>₹{products.price}/-</strong>
                 </div>
 
                 <div className="product-links mt-0">
 
-                <h5 className="card-title p-0 px-1 m-0 text-center" style={{ fontSize: "70%", width:"100%", display:"-webkit-box", overflow:"hidden", WebkitLineClamp:"2", WebkitBoxOrient:"vertical", color:"grey" }}>{products.description}</h5>
+                    <h5 className="card-title p-0 px-1 m-0 text-center" style={{ fontSize: "70%", width: "100%", display: "-webkit-box", overflow: "hidden", WebkitLineClamp: "2", WebkitBoxOrient: "vertical", color: "grey" }}>{products.description}</h5>
 
-						<NavLink style={{marginRight:"3px", cursor:"pointer"}} to={`/details/${products._id}`}>
-                        <div className="text-center" style={{marginTop:"8px"}}>
-                            <button className="btn btn-outline-secondary btn-sm" style={{fontSize:"70%"}}>Veiw Details</button>
+                    <NavLink style={{ marginRight: "3px", cursor: "pointer" }} to={`/details/${products._id}`}>
+                        <div className="text-center" style={{ marginTop: "8px" }}>
+                            <button className="btn btn-outline-secondary btn-sm" style={{ fontSize: "70%" }}>Veiw Details</button>
                         </div>
-						</NavLink>
+                    </NavLink>
 
-						
-					</div>
 
-                    {/* <div className="card-body px-1 py-2" style={{ height:"100%"}}>
+                </div>
+
+                {/* <div className="card-body px-1 py-2" style={{ height:"100%"}}>
                         <div className="clearfix mb-1 d-flex flex-column" style={{width:"100%"}}>
                             <span className=" float-start badge rounded-pill bg-primary" style={{width:"max-content", fontSize:"70%"}}>{products.title}</span>
                             <span className="float-end price-hp mt-1" style={{ fontSize: "70%", width:"max-content", alignSelf:"end" }}>$ {products.price}/-</span>
@@ -44,8 +45,8 @@ const TrendingCard = ({ products }) => {
                         </div>
                         </NavLink>
                     </div> */}
-                </div>
-            
+            </div>
+
 
 
         </div>
