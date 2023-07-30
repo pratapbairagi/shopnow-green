3a7b00f2f7232194afa44906f2503effb311e565
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-07-31 04:16:54 +0530

    testing create product success notification

--- client/src/components/product_notification_toaster/productNotificationToaster.jsx
+++ client/src/components/product_notification_toaster/productNotificationToaster.jsx
@@ -19,9 +19,9 @@ const ProductNotificationRoaster = () => {
             dispatch(get_product_details(id))
         }
 
-    }, [id, dispatch]);
+    }, [id]);
 
-    const clossSuccessAction = () =>{
+    function clossSuccessAction(){
         dispatch(clear_success())
         window.location.href="/profile"
     }
@@ -29,24 +29,20 @@ const ProductNotificationRoaster = () => {
     return (
         <div className="productionNotificationToaster" style={{display:"flex", position:"fixed", top:"0", left:"0", zIndex:"11"}}>
 
-            <button className="btn btn-danger" style={{position:"absolute", top:"10px", right:"10px", zIndex:"12"}} onClick={ ()=> clossSuccessAction}><XLg size="20px"/></button>
+            {/* <button className="btn btn-danger" style={{position:"absolute", top:"10px", right:"10px", zIndex:"12"}} onClick={ ()=> clossSuccessAction}><XLg size="20px"/></button> */}
 
             {/* <div id="snackbar"> */}
             <div className="productionNotificationToaster-product">
                 <div className="productionNotificationToaster-product-img px-2">
                    
-                   {success && product?.images?.map((v,i)=> <img key={i} src={v.url} alt={product.title} /> )}
-                    {/* <img src="https://sslimages.shoppersstop.com/sys-master/images/hb1/h95/29479595311134/S23ATVICENZA748_BLUE.jpg_2000Wx3000H" alt="" />
-                    <img src="https://sslimages.shoppersstop.com/sys-master/images/hb1/h95/29479595311134/S23ATVICENZA748_BLUE.jpg_2000Wx3000H" alt="" />
-
-                    <img src="https://sslimages.shoppersstop.com/sys-master/images/hb1/h95/29479595311134/S23ATVICENZA748_BLUE.jpg_2000Wx3000H" alt="" />
-                    <img src="https://sslimages.shoppersstop.com/sys-master/images/hb1/h95/29479595311134/S23ATVICENZA748_BLUE.jpg_2000Wx3000H" alt="" /> */}
+                   {/* {success && product?.images?.map((v,i)=> <img key={i} src={v.url} alt={product.title} /> )} */}
+                    
                     <span className="productionNotificationToaster-tag">new</span>
                 </div>
                 <div className="productionNotificationToaster-product-listing">
                     <div className="productionNotificationToaster-content">
-                        <h1 className="productionNotificationToaster-name">{product.category}, {product.brand}</h1>
-                        {/* <p className="productionNotificationToaster-info">{product.description}</p> */}
+                        {/* <h1 className="productionNotificationToaster-name">{product.category}, {product.brand}</h1>
+                        <p className="productionNotificationToaster-info">{product.description}</p> */}
                         {/* <p className="productionNotificationToaster-price">$ {product.price}</p> */}
                         <div className="productionNotificationToaster-btn-and-rating-box" style={{ display: "flex", justifyContent: "space-between", flexWrap: "wrap" }}>
                             <div className="productionNotificationToaster-rating" style={{ width: "80%", lineHeight:"175%", textAlign:"left" }}>
@@ -55,25 +51,25 @@ const ProductNotificationRoaster = () => {
                                     <StarFill size="16px"/>
                                     <StarFill size="16px"/>
                                     <StarHalf size="16px"/> */}
-                                {product?.title}
+                                {/* {product?.title} */}
 
                             </div>
-                           <NavLink to={`/details/${id}`} className="productionNotificationToaster-btn"> View </NavLink>
+                           {/* <NavLink to={`/details/${id}`} className="productionNotificationToaster-btn"> View </NavLink> */}
                             <div className="productionNotificationToaster-info mt-1 text-muted" style={{ width: "100%", textAlign: "center" }}>
-                                {product?.description}
+                                {/* {product?.description} */}
                             </div>
                             <div className="productionNotificationToaster-price mt-1" style={{ width: "100%", textAlign: "center" }}>
-                            ₹ { success && product?.price}
+                            {/* ₹ { success && product?.price} */}
                             </div>
                             <div className="productionNotificationToaster-gender mt-1" style={{ width: "100%", textAlign: "left" }}>
-                                {success && product?.gender}
+                                {/* {success && product?.gender} */}
                             </div>
                             <div className="productionNotificationToaster-color mt-1" style={{ width: "100%", textAlign: "left", display: "flex", columnGap: "4px" }}>
                                {success && product?.color?.map((v,i)=> <div key={i} style={{ width: "20px", minHeight: "20px", borderRadius: "50%", background: `${v}` }} ></div>)} 
                                 
                             </div>
                             <div className="productionNotificationToaster-color mt-2" style={{ width: "100%", textAlign: "left", display: "flex", columnGap: "4px" }}>
-                            {success && product?.size?.map((v,i)=>  <div key={i} className="px-2" style={{ width: "max-content", minHeight: "16px", borderRadius: "6px", border:"1px solid white", textTransform:"uppercase" }} >{v}</div> )} 
+                            {/* {success && product?.size?.map((v,i)=>  <div key={i} className="px-2" style={{ width: "max-content", minHeight: "16px", borderRadius: "6px", border:"1px solid white", textTransform:"uppercase" }} >{v}</div> )}  */}
                                
                             </div>
                         </div>
