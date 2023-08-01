c6cd00ed18e9b476f47bf786325e1ea7b3cba985
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-01 19:48:09 +0530

    changed css of gender and category

--- client/src/pages/products_fshop/products_shop.jsx
+++ client/src/pages/products_fshop/products_shop.jsx
@@ -75,7 +75,7 @@ const ProductsShop = () => {
                 if (localStorage.getItem("categories") === undefined || localStorage.getItem("categories") === null) {
                     localStorage.setItem("categories", JSON.stringify(categoryArray))
                 }
-                if (localStorage.getItem("categories") !== undefined || localStorage.getItem("categories") !== null) {
+                if (localStorage.getItem("categories") !== undefined) {
                     if (JSON.parse(localStorage.getItem("categories")).length < categoryArray.length) {
                         localStorage.setItem("categories", JSON.stringify(categoryArray))
                     }
@@ -89,7 +89,7 @@ const ProductsShop = () => {
                 })
                 const sizeArray = Array.from(uniqueSize)
                 // setSizes(sizeArray)
-                if (localStorage.getItem("sizes") === undefined || localStorage.getItem("sizes") === null) {
+                if (localStorage.getItem("sizes") === undefined || localStorage.getItem("sizes") === null || localStorage.getItem("sizes") === null || JSON.parse(localStorage.getItem("sizes"))?.length < sizeArray.length) {
                     localStorage.setItem("sizes", JSON.stringify(sizeArray))
                 }
 
@@ -101,7 +101,7 @@ const ProductsShop = () => {
                 })
                 const colorArray = Array.from(uniqueColor);
                 // setColors(colorArray)
-                if (localStorage.getItem("colors") === undefined || localStorage.getItem("colors") === null) {
+                if (localStorage.getItem("colors") === undefined || localStorage.getItem("colors") === null || JSON.parse(localStorage.getItem("colors"))?.length < colorArray.length) {
                     localStorage.setItem("colors", JSON.stringify(colorArray))
                 }
 
@@ -111,7 +111,7 @@ const ProductsShop = () => {
                 })
                 const brandArray = Array.from(uniqueBrand);
                 // setBrands(brandArray);
-                if (localStorage.getItem("brands") === undefined || localStorage.getItem("brands") === null) {
+                if (localStorage.getItem("brands") === undefined || localStorage.getItem("brands") === null || JSON.parse(localStorage.getItem("brands"))?.length < brandArray.length) {
                     localStorage.setItem("brands", JSON.stringify(brandArray))
                 }
             }
@@ -165,10 +165,10 @@ const ProductsShop = () => {
                                     </header>
                                     <div className={`panel-body  bottom-0 start-0 p-0 m-0 ${toggleFilters === "Gender" ? "d-block" : "d-none"} d-md-block`} style={{ left: "0", minHeight: "max-content", zIndex: "3", height: "max-content", width: "100%" }}>
                                         <ul className="nav prod-cat px-0 m-0" style={{ minHeight: "max-content", height: "max-content", width: "100%" }}>
-                                            <li className="panel-body-li" style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", width: "100%", minWidth: "100%", height: "max-content", minHeight: "max-content", padding: "5% 0 4% 10%", background: "white", gap: "4px" }}>
+                                            <li className="panel-body-li" style={{ display: "flex", flexDirection: "column", alignItems: "flex-start", width: "100%", minWidth: "100%", height: "max-content", minHeight: "max-content", padding: "5% 10% 4% 10%", background: "white", gap: "4px" }}>
 
                                                 {localStorage.getItem("gender") && JSON.parse(localStorage.getItem("gender")).map((v, i) => {
-                                                    return <span key={i} className="p-0 m-0" style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "center", gap: "6px" }}>
+                                                    return <span key={i} className="p-0 m-0" style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", gap: "6px" }}>
                                                         <button className="active d-flex justify-content-center align-items-center p-0" style={{ position: "relative", width: "20px", height: "20px", display: "flex", flexDirection: "column", background: `${search_options.gender !== v.gender ? "white" : "green"}`, color: "white", border: "1px solid grey", borderRadius: "3px" }}>
                                                             <input type="radio" value={v.gender.slice(0,1)} name="category" onChange={(e) => setSearch_options({ ...search_options, gender: e.target.value })} id="" style={{ position: "absolute", width: "100%", height: "100%", opacity: "0" }} />
                                                             {
@@ -199,10 +199,10 @@ const ProductsShop = () => {
                                     </header>
                                     <div className={`panel-body  bottom-0 start-0 p-0 m-0 ${toggleFilters === "Category" ? "d-block" : "d-none"} d-md-block`} style={{ left: "0", minHeight: "max-content", zIndex: "3", height: "max-content", width: "100%", background: "white" }}>
                                         <ul className="nav prod-cat px-0 m-0" style={{ minHeight: "max-content", height: "max-content", width: "100%" }}>
-                                            <li className="panel-body-li" style={{ width: "100%", minWidth: "100%", height: "max-content", minHeight: "20vh", maxHeight: "45vh", overflowY: "auto", padding: "5% 0 4% 10%", background: "white", display: "flex", flexDirection: "column", alignItems: "flex-start", gap: "4px" }}>
+                                            <li className="panel-body-li" style={{ width: "100%", minWidth: "100%", height: "max-content", minHeight: "20vh", maxHeight: "45vh", overflowY: "auto", padding: "5% 10% 4% 10%", background: "white", display: "flex", flexDirection: "column", alignItems: "flex-start", justifyContent:"flex-start", gap: "4px" }}>
 
                                                 {localStorage.getItem("categories") && JSON.parse(localStorage.getItem("categories")).map((c, i) => {
-                                                    return <span className="p-0 m-0" key={i} style={{ display: "flex", flexDirection: "row", justifyContent: "left", alignItems: "center", gap: "6px" }}>
+                                                    return <span className="p-0 m-0" key={i} style={{ display: "flex", flexDirection: "row", justifyContent: "space-between", alignItems: "center", gap: "6px" }}>
                                                         <button className="active d-flex justify-content-center align-items-center p-0" style={{ position: "relative", width: "20px", height: "20px", display: "flex", flexDirection: "column", background: `${search_options.category !== c ? "white" : "green"}`, color: "white", border: "1px solid grey", borderRadius: "3px" }}>
                                                             <input type="radio" value={c} name="category" onChange={(e) => setSearch_options({ ...search_options, category: e.target.value })} id="" style={{ position: "absolute", width: "100%", height: "100%", opacity: "0" }} />
                                                             {
