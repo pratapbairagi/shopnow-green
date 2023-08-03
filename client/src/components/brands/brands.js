df3f16c3e32a01cf5e6a933d4b517a464d93c823
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-03 19:22:30 +0530

    change css of brand section

--- client/src/components/brands/brands.js
+++ client/src/components/brands/brands.js
@@ -24,14 +24,13 @@ const Brands = () => {
 
     return (
         <div className="p-0 m-0 w-100 bg-light pt-2 pb-1" style={{ height: "max-content" }}>
-            <h5 className="mt-2" style={{ margin: "auto", marginTop: "10px", borderBottom: "2px solid rgb(238, 236, 236)", width: "max-content", color: "grey", fontSize: "80%" }}>Our Brands</h5>
-            <div className="container brand_container mt-1 px-2" style={{ background: "white" }}>
+            <h5 className=" m-0 mt-2" style={{ margin: "auto", marginTop: "10px", borderBottom: "2px solid rgb(238, 236, 236)", width: "max-content", color: "grey", fontSize: "80%" }}>Our Brands</h5>
+            <div className="container brand_container mt-1 px-2 py-0">
                 {/* <div className="card"> */}
 
                 { productsFilters !== null && productsFilters !== undefined && productsFilters.length > 0 && productsFilters.map((v,i)=>{ 
-           return <div key={i} onClick={()=> navigate(`/${v}`) } className="face face1 m-0" style={{border:"1px solid rgb(238, 236, 236)", height:"max-content", padding:"6px 10px", cursor:"pointer"}}>
+           return <div key={i} onClick={()=> navigate(`/${v}`) } className="face face1 m-0" style={{border:"1px solid rgb(238, 236, 236)", height:"max-content", padding:"6px 10px", cursor:"pointer", background: "white"}}>
                 <div className="content">
-                    <img style={{ maxHeight:"50px"}} src="https://brandongaille.com/wp-content/uploads/2013/06/H-and-M-Company-Logo-Image.jpg" alt="brand 1" />
                     <h6 className="p-0 m-0 text-muted" style={{width:"max-content", whiteSpace:"nowrap", textAlign:"center", fontSize:"80%"}}>{v}</h6>
                 </div>
             </div> })
