2ad7099738c744a5be001348d66375cfe1654caa
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-03 19:32:14 +0530

    change css of brand section

--- client/src/components/brands/brands.js
+++ client/src/components/brands/brands.js
@@ -24,14 +24,14 @@ const Brands = () => {
 
     return (
         <div className="p-0 m-0 w-100 bg-light pt-2 pb-1" style={{ height: "max-content" }}>
-            <h5 className=" m-0 mt-2" style={{ margin: "auto", marginTop: "10px", borderBottom: "2px solid rgb(238, 236, 236)", width: "max-content", color: "grey", fontSize: "80%" }}>Our Brands</h5>
-            <div className="container brand_container mt-1 px-2 py-0">
+            <h5 className="" style={{ margin: "10px auto", borderBottom: "2px solid rgb(238, 236, 236)", width: "max-content", color: "grey", fontSize: "80%" }}>Our Brands</h5>
+            <div className="container brand_container m-0 px-2 py-0">
                 {/* <div className="card"> */}
 
                 { productsFilters !== null && productsFilters !== undefined && productsFilters.length > 0 && productsFilters.map((v,i)=>{ 
-           return <div key={i} onClick={()=> navigate(`/${v}`) } className="face face1 m-0" style={{border:"1px solid rgb(238, 236, 236)", height:"max-content", padding:"6px 10px", cursor:"pointer", background: "white"}}>
+           return <div key={i} onClick={()=> navigate(`/${v}`) } className="face face1 m-0" style={{border:"1px solid rgb(238, 236, 236)", height:"max-content", padding:"6px 10px", cursor:"pointer", background: "white", borderRadius:"4px"}}>
                 <div className="content">
-                    <h6 className="p-0 m-0 text-muted" style={{width:"max-content", whiteSpace:"nowrap", textAlign:"center", fontSize:"80%"}}>{v}</h6>
+                    <h6 className="p-0 m-0 text-muted" style={{width:"max-content", whiteSpace:"nowrap", textAlign:"center", fontSize:"80%"}}>{v.toUpperCase()}</h6>
                 </div>
             </div> })
             }
