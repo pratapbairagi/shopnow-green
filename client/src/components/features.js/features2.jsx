202b01b2094602512a6dfd2d7a42b3afc7a0ab60
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-04 23:19:30 +0530

    added features2 component

--- client/src/components/features.js/features2.jsx
+++ client/src/components/features.js/features2.jsx
@@ -7,10 +7,10 @@ const Features2 = () => {
         <div style={{width:"100%", height:"max-content", marginTop:"20px"}}>
 
                     <h6 className="m-auto" style={{ width: "100%", textAlign: "center", fontSize:"110%", fontWeight:"700", color:"grey" }}>Why Shop Whith Us ?</h6>
-                    <p className="mt-2 px-2" style={{ width: "100%", maxWidth:"100%", textAlign: "center", fontSize:"100%", fontWeight:"500", lineHeight:"120%", color:"grey" }}>An impressive wonderful example of a particular quality type of idea</p>
+                    <p className="mt-2 px-2" style={{ width: "100%", maxWidth:"100%", textAlign: "center", fontSize:"100%", fontWeight:"400", lineHeight:"120%", color:"grey" }}>An impressive wonderful example of a particular quality type of idea</p>
                     <div className="row p-2 gap-0 m-auto">
                         
-                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"max-content", height:"max-content"}}>
+                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"165px", height:"max-content"}}>
                             <div className="card-body d-flex flex-column align-items-center mb-0 p-3 border rounded-3">
                                 <StarHalf size="33px" fill="red" />
                                 <div className=" mt-2 mb-1 text-center" style={{border:"none", fontWeight:"700", letterSpacing:".5px", fontSize:"16px"}}>
@@ -20,7 +20,7 @@ const Features2 = () => {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"max-content", height:"max-content"}}>
+                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"165px", height:"max-content"}}>
                             <div className="card-body d-flex flex-column align-items-center mb-0 p-3 border rounded-3">
                                 <HousesFill size="33px" fill="red" />
                                 <div className=" mt-2 mb-1 text-center" style={{border:"none", fontWeight:"700", letterSpacing:".5px", fontSize:"16px"}}>
@@ -30,7 +30,7 @@ const Features2 = () => {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"max-content", height:"max-content"}}>
+                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"165px", height:"max-content"}}>
                             <div className="card-body d-flex flex-column align-items-center mb-0 p-3 border rounded-3">
                                 <Truck size="33px" fill="red" />
                                 <div className=" mt-2 mb-1 text-center" style={{border:"none", fontWeight:"700", letterSpacing:".5px", fontSize:"16px"}}>
@@ -40,7 +40,7 @@ const Features2 = () => {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"max-content", height:"max-content"}}>
+                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"165px", height:"max-content"}}>
                             <div className="card-body d-flex flex-column align-items-center mb-0 p-3 border rounded-3">
                                 <ShieldFillCheck size="33px" fill="red" />
                                 <div className=" mt-2 mb-1 text-center" style={{border:"none", fontWeight:"700", letterSpacing:".5px", fontSize:"16px"}}>
@@ -50,7 +50,7 @@ const Features2 = () => {
                             </div>
                         </div>
 
-                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"max-content", height:"max-content"}}>
+                        <div className="col-12 col-md-col-6 col-lg-4 col-xl-3 card p-1" style={{border:"none", maxHeight:"165px", height:"max-content"}}>
                             <div className="card-body d-flex flex-column align-items-center mb-0 p-3 border rounded-3">
                                 <QuestionCircleFill size="33px" fill="red" />
                                 <div className=" mt-2 mb-1 text-center" style={{border:"none", fontWeight:"700", letterSpacing:".5px", fontSize:"16px"}}>
