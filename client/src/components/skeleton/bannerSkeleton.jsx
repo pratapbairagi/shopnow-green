726f47f94badf92ba3b71af2915bfc8c73c0baf4
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-06 17:33:28 +0530

    category, banner and features skeleton adding

--- client/src/components/skeleton/bannerSkeleton.jsx
+++ client/src/components/skeleton/bannerSkeleton.jsx
@@ -6,11 +6,11 @@ const BannerSkeleton = () => {
 
                 <div className="col order-1 order-sm-1 order-md-0 col-12 col-md-6 col-lg-4 mx-auto bannerSkeletonCol1" >
                     <div className="d-flex flex-column align-items-center align-items-md-start justify-content-md-center mt-0 p-0 px-md-5 py-4" style={{ height:"100%"}}>
-                    <div className="bannerSkeleton" style={{width:"180px", minHeight:"2.5vh", borderRadius:"4px"}}></div>
-                        <h1 className="text-center text-md-start bannerSkeleton" style={{minWidth:"220px", marginTop:"10px", height:"5vh", borderRadius:"4px"}}> </h1>
-                        <h1 className="text-center text-md-start bannerSkeleton" style={{minWidth:"200px", marginTop:"4px", height:"5vh", borderRadius:"4px"}}> </h1>
+                    <div className="bannerSkeleton" style={{width:"170px", minHeight:"2.5vh", borderRadius:"4px"}}></div>
+                        <h1 className="text-center text-md-start bannerSkeleton" style={{minWidth:"210px", marginTop:"10px", height:"4vh", borderRadius:"4px"}}> </h1>
+                        <h1 className="text-center text-md-start bannerSkeleton" style={{minWidth:"190px", marginTop:"0px", height:"4vh", borderRadius:"4px"}}> </h1>
 
-                        <div className=" rounded-0 bannerSkeleton" style={{ marginLeft: "8px", marginTop: "14px", border:"none", minWidth:"110px", height:"4.7vh" }}></div>
+                        <div className=" rounded-0 bannerSkeleton" style={{ marginLeft: "8px", marginTop: "10px", border:"none", minWidth:"100px", height:"4.7vh" }}></div>
                     </div>
                 </div>
 
