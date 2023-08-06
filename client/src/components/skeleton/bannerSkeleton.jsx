40b24af4a5c33a29f6c59af9ca5e6c71ced3e78a
PratapBairagi2 <pratap.bairagi.test@gmail.com>
2023-08-06 17:22:12 +0530

    category, banner and features skeleton adding

--- client/src/components/skeleton/bannerSkeleton.jsx
+++ client/src/components/skeleton/bannerSkeleton.jsx
@@ -1,10 +1,10 @@
-
+import "./bannerSkeleton.css"
 
 const BannerSkeleton = () => {
     return (
         <div className="row px-4 py-1 d-flex" style={{maxWidth:"100%", marginLeft:"2px", background:"rgb(253, 249, 243)"}}>
 
-                <div className="col order-1 order-sm-1 order-md-0 col-12 col-md-6 col-lg-4 mx-auto" >
+                <div className="col order-1 order-sm-1 order-md-0 col-12 col-md-6 col-lg-4 mx-auto bannerSkeletonCol1" >
                     <div className="d-flex flex-column align-items-center align-items-md-start justify-content-md-center mt-0 p-0 px-md-5 py-4" style={{ height:"100%"}}>
                     <div className="bannerSkeleton" style={{width:"180px", minHeight:"2.5vh", borderRadius:"4px"}}></div>
                         <h1 className="text-center text-md-start bannerSkeleton" style={{minWidth:"220px", marginTop:"10px", height:"5vh", borderRadius:"4px"}}> </h1>
@@ -14,7 +14,7 @@ const BannerSkeleton = () => {
                     </div>
                 </div>
 
-                <div className="col order-0 order-sm-0 order-md-1 col-12 col-md-6 col-lg-8 bannerSkeleton d-flex justify-content-center" style={{ position: "relative", minHeight:"68vh" }}>
+                <div className="col order-0 order-sm-0 order-md-1 col-12 col-md-6 col-lg-8 bannerSkeleton d-flex justify-content-center bannerSkeletonCol2" style={{ position: "relative" }}>
 
                     
 
